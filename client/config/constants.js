01407add27c223e1b9b58604d057000cd5178979
Chyld Medford <chyld.medford@gmail.com>
2015-05-07 12:44:51 -0700

    Update constants.js

--- client/config/constants.js
+++ client/config/constants.js
@@ -1,5 +1,5 @@
 'use strict';
 
 angular.module('poseidon')
-.constant('firebaseUrl', 'https://poseidon-aphrodite.firebaseio.com/')
+.constant('firebaseUrl', 'https://_________.firebaseio.com/')
 .constant('nodeUrl', 'http://localhost:8000');
